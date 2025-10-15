4d05530617e906fa4e6a75d726b0be8a754b92c3
Xi Luo <xi.rossi.luo@gmail.com>
2025-10-15 15:35:12 -0500

    ignor ob

--- R/cord.R
+++ R/cord.R
@@ -1,8 +1,8 @@
 ##' @title Community estimation in G-models via CORD
 ##'
-##' @description Partition data points (variables) into clusters/communities. Reference: Bunea, F., Giraud, C., & Luo, X. (2015). Community estimation in \eqn{G}-models via CORD. arXiv preprint arXiv:1508.01939. \url{http://arxiv.org/abs/1508.01939}.
+##' @description Partition data points (variables) into clusters/communities. Reference: Bunea et al (2020). Model assisted variable clustering: Minimax-optimal recovery and algorithms, Annals of Statistics, \url{https://doi.org/10.1214/18-AOS1794}.
 ##'
-##' 
+##'
 ##' @param X Input data matrix. It should be an n (samples) by p (variables) matrix  when \code{input} is set to the value "data" by default. It can also be a p by p symmetric matrix when \code{X} is a correlation matrix or a distance matrix if \code{input} is set accordingly.
 ##' @param tau Threshold to use at each iteration. A theoretical choice is about \eqn{2n^{-1/2}\log^{1/2} p}.
 ##' @param kendall Whether to compute Kendall's tau correlation matrix from \code{X}, when \code{input} is set to "data". If \code{FALSE}, Pearson's correlation will be computed, usually faster for large p.
@@ -10,20 +10,19 @@
 ##' @return \code{list} with one element: a vector of integers showing which cluster/community each point is assigned to.
 ##'
 ##' @importFrom stats cor
-##' 
+##'
 ##' @examples
 ##' set.seed(100)
 ##' X <- 2*matrix(rnorm(200*2), 200, 10)+matrix(rnorm(200*10), 200, 10)
 ##' cord(X)
 ##'
 ##' @keywords multivariate cluster
-##' 
-##' @export 
-cord <- function(X, tau=2*sqrt(log(ncol(X))/nrow(X)), kendall=T,  input=c("data", "cor", "dist")) {
-    
-    input <- match.arg(input,c("data", "cor", "dist"))
+##'
+##' @export
+cord <- function(X, tau = 2 * sqrt(log(ncol(X)) / nrow(X)), kendall = T, input = c("data", "cor", "dist")) {
+    input <- match.arg(input, c("data", "cor", "dist"))
 
-    if(input == "cor") {
+    if (input == "cor") {
         input.cordDist <- F
         input.cor <- T
     } else if (input == "cordDist") {
@@ -33,14 +32,14 @@ cord <- function(X, tau=2*sqrt(log(ncol(X))/nrow(X)), kendall=T,  input=c("data"
         input.cordDist <- F
         input.cor <- F
     }
-     n <- nrow(X)
+    n <- nrow(X)
     p <- ncol(X)
 
     mat <- NULL
-    
-    
+
+
     if (input.cordDist) {
-        fd <- X 
+        fd <- X
     } else {
         if (input.cor) {
             mat <- X
@@ -49,21 +48,19 @@ cord <- function(X, tau=2*sqrt(log(ncol(X))/nrow(X)), kendall=T,  input=c("data"
                 ## compute distance matrix
                 if (kendall) {
                     ## mat <- sin(pi*cor.fk(X)/2)
-                    mat <- sin(pi*cor(X, method= "kendall")/2)
+                    mat <- sin(pi * cor(X, method = "kendall") / 2)
                 } else {
-                    mat <- cor(X) 
+                    mat <- cor(X)
                 }
             }
         }
         fd <- cordDist(mat)
     }
-    
+
     ## add large values to the diagonal
-    fd <- fd+diag(1e+5, p) 
-    
+    fd <- fd + diag(1e+5, p)
+
     Gvec <- greedyCord(fd, tau)
-    
-    return(list(cluster=as.numeric(Gvec)))
+
+    return(list(cluster = as.numeric(Gvec)))
 }
-
-
